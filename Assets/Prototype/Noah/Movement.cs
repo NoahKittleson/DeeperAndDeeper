ba666fad9846c1c197d45664c437bfddfd7ed44e
Noah Kittleson <noahkittleson@gmail.com>
2021-06-05 14:06:37 -0700

    basic bouncing

--- Assets/Prototype/Noah/Movement.cs
+++ Assets/Prototype/Noah/Movement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Vector2 lastVelocity;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +17,19 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        lastVelocity = GetComponent<Rigidbody2D>().velocity;
         // if(Input.GetKeyDown(KeyCode.Space)) {
         //     Debug.Log("I did it!");
         //     Move(new Vector2(-10, -10), 1);
         // }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if we want to collide with the object
+        var speed = lastVelocity.magnitude;
+        var direction = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
     }
 
     public void Move(Vector2 mousePosition, float power) {
